9122d1fc5cc5a245dd44572fb54960495f50682a
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-13 11:55:04 -0300

    Projeto de console para calcular aumento finalizado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calcular_Aumento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            double salfinal;
+            double i = 0.30;
+            
+            Console.WriteLine("Para saber se tem direito ao aumento, digite seu salário");
+            salario = double.Parse(Console.ReadLine());
+            
+            salfinal = salario + (salario*i);
+
+            if(salario< 500){
+              Console.WriteLine("Você recebeu o aumento de 30%. Seu novo salário é :"+salfinal);
+            }else{
+                Console.WriteLine("Você não tem direito ao aumento.");
+            }
+        }
+    }
+}
